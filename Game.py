2b61b720e60c4dafa648a8a75a46de07154dae9b
mpino <mpino1701@gmail.com>
2021-10-08 22:13:02 -0300

    sistema de widgets en pantalla

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+from kivy.animation import Animation
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
@@ -17,9 +18,24 @@ class Game(Screen):
         self.relacion_aspecto ={"cuadroX":64,"cuadroY":96}
         self.lista_objetos = Lista_Objetos()
         
+        temporalX = self.relacion_aspecto["cuadroX"] /2
+        temporalY = self.relacion_aspecto["cuadroY"] / 3
         
+        guiaX = 0
+        guiaY = 0
+        guia = 0
+        
+        for y in range(0,30):
+            for x in range(0,20):
+                self.ventana.add_widget(self.lista_objetos.get_objeto(1,self.relacion_aspecto,[guiaX,guiaY]))
+                guiaX +=temporalX
+                guiaY += temporalY
+            guiaX = y*temporalX
+            guia = 0
+            guia -= y*temporalY
+            guiaY = guia
+                
         
     def update(self,*dt):
         self.relacion_aspecto.update(SizeTools.set_aspecto(Window.size,25,14))
-        print(self.relacion_aspecto)
         
